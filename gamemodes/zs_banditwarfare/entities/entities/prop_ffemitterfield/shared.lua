106af4a0f8766608fbb1cb2674f3d2005fe9a0e5
AirRice <jpumph2@gmail.com>
2023-01-29 14:35:45 +0000

    More custom collision fixes

--- gamemodes/zs_banditwarfare/entities/entities/prop_ffemitterfield/shared.lua
+++ gamemodes/zs_banditwarfare/entities/entities/prop_ffemitterfield/shared.lua
@@ -5,7 +5,7 @@ ENT.CanPackUp = true
 ENT.m_NoNailUnfreeze = true
 ENT.NoNails = true
 
-function ENT:ShouldNotCollide(ent)
+function ENT:CustomNoCollideRules(ent)
 	if ent:IsProjectile() then 
 		return true 
 	elseif (ent:GetClass() == "prop_drone") or (ent:GetClass() == "prop_manhack") then
