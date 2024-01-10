d9b23337df4b210681ce37a710df340f005f35e5
JOOHO LEE <2106655@uad.ac.uk>
2024-01-10 12:48:08 +0000

    Have hook stay in inventory but be unusable instead of being removed (so it can be upgraded)

--- gamemodes/zs_banditwarfare/entities/entities/prop_meathook/init.lua
+++ gamemodes/zs_banditwarfare/entities/entities/prop_meathook/init.lua
@@ -16,39 +16,36 @@ end
 function ENT:Think()
 	local parent = self:GetParent()
 	local ownerteam = self:GetOwner():IsPlayer() and self:GetOwner():Team() or nil
-	if parent:IsValid() and parent:IsPlayer() then
-		if parent:Alive() and parent:Team() ~= ownerteam and self.TicksLeft >= 1 then
-			if CurTime() >= self.NextDamage then
-				self.NextDamage = CurTime() + 0.5
-				self.TicksLeft = self.TicksLeft - 1
+	if parent:IsValid() and parent:IsPlayer() and parent:Alive() and parent:Team() ~= ownerteam and self.TicksLeft >= 1 then
+		if CurTime() >= self.NextDamage then
+			self.NextDamage = CurTime() + 0.5
+			self.TicksLeft = self.TicksLeft - 1
 
-				util.Blood((parent:NearestPoint(self:GetPos()) + parent:WorldSpaceCenter()) / 2, math.random(4, 9), Vector(0, 0, 1), 100)
-				parent:TakeDamage(8, self:GetOwner(), self)
-			end
-		else
-			if GAMEMODE:IsClassicMode() then
-				local ent = ents.Create("prop_weapon")
-				if ent:IsValid() then
-					ent:SetWeaponType("weapon_zs_hook")
-					ent:SetPos(self:GetPos())
-					ent:SetAngles(self:GetAngles())
-					ent:Spawn()
-					local phys = ent:GetPhysicsObject()
-					if phys:IsValid() then
-						phys:Wake()
-						phys:AddAngleVelocity(VectorRand() * 200)
-						phys:SetVelocityInstantaneous(Vector(0, 0, 200) + parent:GetVelocity())
-					end
-				end
-			elseif self:GetOwner():IsPlayer() and self:GetOwner():GetWeaponMelee() == self.WeaponClass and self:GetOwner():Alive() then
-				local originalwep = self:GetOwner():GetWeapon(self.WeaponClass)
-				if not (originalwep and originalwep:IsValid()) then
-					self:GetOwner():Give(self.WeaponClass)
-				end
-			end
-			self:Remove()
+			util.Blood((parent:NearestPoint(self:GetPos()) + parent:WorldSpaceCenter()) / 2, math.random(4, 9), Vector(0, 0, 1), 100)
+			parent:TakeDamage(8, self:GetOwner(), self)
+			parent:AddLegDamage(5)
 		end
 	else
+		/*if GAMEMODE:IsClassicMode() then
+			local ent = ents.Create("prop_weapon")
+			if ent:IsValid() then
+				ent:SetWeaponType("weapon_zs_hook")
+				ent:SetPos(self:GetPos())
+				ent:SetAngles(self:GetAngles())
+				ent:Spawn()
+				local phys = ent:GetPhysicsObject()
+				if phys:IsValid() then
+					phys:Wake()
+					phys:AddAngleVelocity(VectorRand() * 200)
+					phys:SetVelocityInstantaneous(Vector(0, 0, 200) + parent:GetVelocity())
+				end
+			end
+		elseif self:GetOwner():IsPlayer() and self:GetOwner():GetWeaponMelee() == self.WeaponClass and self:GetOwner():Alive() then
+			local originalwep = self:GetOwner():GetWeapon(self.WeaponClass)
+			if not (originalwep and originalwep:IsValid()) then
+				self:GetOwner():Give(self.WeaponClass)
+			end
+		end*/
 		self:Remove()
 	end
 end
--- gamemodes/zs_banditwarfare/entities/weapons/weapon_zs_hook.lua
+++ gamemodes/zs_banditwarfare/entities/weapons/weapon_zs_hook.lua
@@ -39,6 +39,16 @@ SWEP.SwingHoldType = "grenade"
 
 SWEP.IsConsumable = true
 
+SWEP.RegenTime = 5
+
+function SWEP:SetupDataTables()
+	self:NetworkVar("Bool", 31, "IsHit")
+	self:NetworkVar("Float", 31, "LastHit")
+	if self.BaseClass.SetupDataTables then
+		self.BaseClass.SetupDataTables(self)
+	end
+end
+
 function SWEP:PlaySwingSound()
 	self:EmitSound("weapons/iceaxe/iceaxe_swing1.wav", 75, math.random(95, 105))
 end
@@ -59,7 +69,10 @@ function SWEP:PlayerHitUtil(owner, damage, hitent, dmginfo)
 			return false
 		end
 		hitent:AddLegDamage(50)
-
+		if not self:GetIsHit() then
+			self:SetIsHit(true)
+			self:SetLastHit(CurTime())
+		end
 		if SERVER then
 			local ang = self:GetOwner():EyeAngles()
 			ang:RotateAroundAxis(ang:Forward(), 180)
@@ -91,7 +104,77 @@ function SWEP:PlayerHitUtil(owner, damage, hitent, dmginfo)
 
 				ent:SetAngles(ang)
 			end
-			timer.Simple(0, function() owner:StripWeapon(self:GetClass()) end)
+			--timer.Simple(0, function() owner:StripWeapon(self:GetClass()) end)
+		end
+		self:SetIsHit(true)
+	end
+end
+
+function SWEP:CanPrimaryAttack()
+	if self:IsHit() then return false end
+
+	return self.BaseClass.CanPrimaryAttack(self)
+end
+
+function SWEP:Think()
+	if self:GetLastHit() and self:GetLastHit() != 0 and self:GetLastHit() + self.RegenTime <= CurTime() then
+		self:GiveNewHook()
+		self:SetLastHit(0)
+	end
+	self.BaseClass.Think(self)
+end
+
+function SWEP:GiveNewHook()
+	self:SendWeaponAnim(ACT_VM_DRAW)
+	self.IdleAnimation = CurTime() + self:SequenceDuration()
+	self:SetNextPrimaryFire(self.IdleAnimation)
+	self:SetIsHit(false)
+end
+
+if CLIENT then
+	function SWEP:PostDrawViewModel(vm, pl, wep)
+		local veles = self.VElements
+		local dontdraw = self:GetIsHit()
+		local col1, col2 = Color(0, 0, 0, 0), Color(255, 255, 255, 255)
+
+		veles["base"].color = dontdraw and col1 or col2
+
+		if self.BaseClass.PostDrawViewModel then 
+			self.BaseClass.PostDrawViewModel(self,vm, pl, wep)
+		end
+	end
+
+	function SWEP:DrawWorldModel()
+		local owner = self:GetOwner()
+		if owner:IsValid() and owner.ShadowMan then return end
+		local weles = self.WElements
+		local dontdraw = self:GetIsHit()
+		local col1, col2 = Color(0, 0, 0, 0), Color(255, 255, 255, 255)
+
+		weles["base"].color = dontdraw and col1 or col2
+
+		self:Anim_DrawWorldModel()
+	end
+
+	local texGradDown = surface.GetTextureID("VGUI/gradient_down")
+	function SWEP:DrawHUD()
+		local scrW = ScrW()
+		local scrH = ScrH()
+		local width = 200
+		local height = 30
+		local x, y = ScrW() - width - 32, ScrH() - height - 72
+		local ratio = math.Clamp((CurTime()-self:GetLastHit())/self.RegenTime,0,1) 
+		if ratio > 0 and self:GetIsHit() then
+			surface.SetDrawColor(5, 5, 5, 180)
+			surface.DrawRect(x, y, width, height)
+			surface.SetDrawColor(255, 0, 0, 180)
+			surface.SetTexture(texGradDown)
+			surface.DrawTexturedRect(x, y, width*ratio, height)
+			surface.SetDrawColor(255, 0, 0, 180)
+			surface.DrawOutlinedRect(x - 1, y - 1, width + 2, height + 2)
+		end
+		if self.BaseClass.DrawHUD then
+			self.BaseClass.DrawHUD(self)
 		end
 	end
 end
